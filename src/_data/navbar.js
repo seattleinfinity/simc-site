d10cc5aec9ff25cd73b3db320f3928a2d0aab544
William Y. Feng <womogenes2@gmail.com>
2022-08-28 23:18:36 -0700

    Change up some colors

---
+++ src/_data/navbar.js
@@ -0,0 +1,30 @@
+module.exports = [
+  /* {
+    title: 'Magazine',
+    url: '/magazine',
+  }, */
+  {
+    title: 'Press Releases',
+    url: '/press-releases',
+  },
+  {
+    title: 'PoTM',
+    url: '/potm',
+  },
+  {
+    title: 'Events',
+    url: '/events',
+  },
+  {
+    title: 'Resources',
+    url: '/resources',
+  },
+  {
+    title: 'SLG',
+    url: '/slg',
+  },
+  {
+    title: 'Contact',
+    url: '/contact',
+  },
+];
