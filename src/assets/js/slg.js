3113a2568f1bd49c9a7c39fb5df8a220e526eaf4
alonr619 <alan.ragoler@gmail.com>
2023-08-10 22:44:04 -0700

    SLG biosgit add .

---
+++ src/assets/js/slg.js
@@ -0,0 +1,10 @@
+function updatePersonHeight(index, isEnter) {
+  let container = document.getElementById(`container-${index}`);
+  if (isEnter) {
+    let back = document.getElementById(`back-${index}`);
+    container.style.height = (back.clientHeight + 50).toString() + "px";
+  } else {
+    let front = document.getElementById(`front-${index}`);
+    container.style.height = (front.clientHeight + 100).toString() + "px";
+  }
+}
