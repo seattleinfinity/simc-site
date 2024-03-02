2bb342dd5265ebdb1efb1a110e6320cc0975e8ae
William Y. Feng <womogenes2@gmail.com>
2024-03-02 15:11:57 -0500

    Make circle article rendering more robust and fix press release photos

--- src/_data/circleArticles.js
+++ src/_data/circleArticles.js
@@ -23,102 +23,106 @@ const fetchContents = async () => {
     .map((dir) => dir.name);
   console.log(allIssues);
 
-  let articlesByIssue = await Promise.all(
-    allIssues.map(async (issueName) => {
-      // Get the full name of this issue (e.g. "nov22" -> "November 2022")
-      const [_, monthAbbrev, year] = /^([a-zA-Z]+)(\d+)$/g.exec(issueName);
-      const monthNames = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ];
-      const monthIndex = monthNames.findIndex((x) =>
-        x.toLowerCase().startsWith(monthAbbrev)
-      );
-      const issueFullName = `${monthNames[monthIndex]} 20${year}`;
-      const issueDate = `20${year}-${(monthIndex + 1)
-        .toString()
-        .padStart(2, '0')}`;
+  let articlesByIssue = (
+    await Promise.all(
+      allIssues.map(async (issueName) => {
+        // Get the full name of this issue (e.g. "nov22" -> "November 2022")
+        let extractIssueInfo = /^([a-zA-Z]+)(\d+)$/g.exec(issueName);
+        if (!extractIssueInfo) return;
+        const [_, monthAbbrev, year] = extractIssueInfo;
+        const monthNames = [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ];
+        const monthIndex = monthNames.findIndex((x) =>
+          x.toLowerCase().startsWith(monthAbbrev)
+        );
+        const issueFullName = `${monthNames[monthIndex]} 20${year}`;
+        const issueDate = `20${year}-${(monthIndex + 1)
+          .toString()
+          .padStart(2, '0')}`;
 
-      // Get contents
-      const url = `${contentsURL}/${issueName}`;
-      const data = await EleventyFetch(url, eleventyFetchOptions('json'));
+        // Get contents
+        const url = `${contentsURL}/${issueName}`;
+        const data = await EleventyFetch(url, eleventyFetchOptions('json'));
 
-      const articles = await Promise.all(
-        data
-          .filter((object) => object.type === 'file')
-          .map(async (object) => {
-            let content = await EleventyFetch(
-              object.download_url,
-              eleventyFetchOptions('text')
-            );
+        const articles = await Promise.all(
+          data
+            .filter((object) => object.type === 'file')
+            .map(async (object) => {
+              let content = await EleventyFetch(
+                object.download_url,
+                eleventyFetchOptions('text')
+              );
 
-            // Extract body, author, etc. from tex source
-            let body = /\\begin{document}([\s\S]+)\\end{document}/g
-              .exec(content)[1]
-              .replace(/\\maketitle\s*|\\section{.+?}\s*/, '');
-            const author = /\\author{([\s\S]+?)}/g.exec(content)[1];
-            const title = /\\title{([\s\S]+?)}/g.exec(content)[1];
+              // Extract body, author, etc. from tex source
+              let body = /\\begin{document}([\s\S]+)\\end{document}/g
+                .exec(content)[1]
+                .replace(/\\maketitle\s*|\\section{.+?}\s*/, '');
+              const author = /\\author{([\s\S]+?)}/g.exec(content)[1];
+              const title = /\\title{([\s\S]+?)}/g.exec(content)[1];
 
-            let blurb = /\\blurb{(.+)}\n/g.exec(content);
-            blurb = blurb ? blurb[1] : blurbify(body);
+              let blurb = /\\blurb{(.+)}\n/g.exec(content);
+              blurb = blurb ? blurb[1] : blurbify(body);
 
-            let epigraph = /\\epigraph{([^]+?)}{(.+)}/g.exec(content);
-            epigraph = epigraph
-              ? epigraph.slice(1, 3).map((x) => latexFilter(x))
-              : '';
+              let epigraph = /\\epigraph{([^]+?)}{(.+)}/g.exec(content);
+              epigraph = epigraph
+                ? epigraph.slice(1, 3).map((x) => latexFilter(x))
+                : '';
 
-            // Render the Latex
-            body = latexFilter(body);
+              // Render the Latex
+              body = latexFilter(body);
 
-            // Replace all images
-            body = body.replace(
-              /(?:\\begin{center})?\s*\\includegraphics(?:\[.*?(?:width=(.+))?.*?\])?{(.+?)}\s*(?:\\end{center})?/g,
-              (_, width, url) => {
-                // Extract width from \includegraphics syntax
-                let widthProp = width
-                  ? `style="width: ${width.split(',')[0]}"`
-                  : '';
+              // Replace all images
+              body = body.replace(
+                /(?:\\begin{center})?\s*\\includegraphics(?:\[.*?(?:width=(.+))?.*?\])?{(.+?)}\s*(?:\\end{center})?/g,
+                (_, width, url) => {
+                  // Extract width from \includegraphics syntax
+                  let widthProp = width
+                    ? `style="width: ${width.split(',')[0]}"`
+                    : '';
 
-                // Set up image source
-                let ghImgBase =
-                  'https://raw.githubusercontent.com/seattleinfinity/simc-circle-articles/main';
-                return `\n\n<img src="${ghImgBase}/${issueName}/${url}" class="my-4" ${widthProp} alt="${url}" />\n\n`;
-              }
-            );
+                  // Set up image source
+                  let ghImgBase =
+                    'https://raw.githubusercontent.com/seattleinfinity/simc-circle-articles/main';
+                  return `\n\n<img src="${ghImgBase}/${issueName}/${url}" class="my-4" ${widthProp} alt="${url}" />\n\n`;
+                }
+              );
 
-            // Get a cover image
-            let titleHash = cyrb53(`${title}-${author}`) % 1003;
-            let coverImage = /<img.*?src="(.+?)"/g.exec(body);
-            coverImage = coverImage
-              ? coverImage[1]
-              : `https://loremflickr.com/1280/720/abstract?lock=${titleHash}`;
+              // Get a cover image
+              let titleHash = cyrb53(`${title}-${author}`) % 1003;
+              let coverImage = /<img.*?src="(.+?)"/g.exec(body);
+              coverImage = coverImage
+                ? coverImage[1]
+                : `https://loremflickr.com/1280/720/abstract?lock=${titleHash}`;
 
-            return {
-              body,
-              author,
-              title,
-              coverImage,
-              issue: issueName,
-              issueFullName,
-              blurb,
-              epigraph,
-            };
-          })
-      );
+              return {
+                body,
+                author,
+                title,
+                coverImage,
+                issue: issueName,
+                issueFullName,
+                blurb,
+                epigraph,
+              };
+            })
+        );
 
-      return { issue: issueName, issueDate, issueFullName, articles };
-    })
-  );
+        return { issue: issueName, issueDate, issueFullName, articles };
+      })
+    )
+  ).filter((article) => article); // Make sure articles are not null
 
   // Combine them in one big array
   // This is necessary for Eleventy's pagination
